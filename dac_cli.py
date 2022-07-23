f1131d523507fea450a9d1023520aeb5ff0afc9f
Hiroki Fujii <hfujii@hisystron.com>
2022-07-23 19:03:18 +0900

    CLIの実装

---
+++ dac_cli.py
@@ -0,0 +1,16 @@
+import daisyMaker
+import time
+
+makerThread = daisyMaker.daisyMaker("input.epub")
+print("created", flush=True)
+makerThread.start()
+print("started", flush=True)
+
+closing = False
+while makerThread.finished == False:
+    time.sleep(0.1)
+    if closing == False: print("%d / %d processed." %(makerThread.count, makerThread.total), end="\r", flush=True)
+    if makerThread.count == makerThread.total and makerThread.total >= 1 and closing == False:
+        print("\nclosing...", flush=True)
+        closing = True
+
