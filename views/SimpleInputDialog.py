c1365b93976c05c78a77ece7b2d014dbe436bd0f
Hiroki Fujii <hfujii@hisystron.com>
2022-07-23 23:08:41 +0900

    ViewKit適用

---
+++ views/SimpleInputDialog.py
@@ -0,0 +1,48 @@
+﻿# -*- coding: utf-8 -*-
+# Simple input dialog view
+# Copyright (C) 2019-2020 yamahubuki <itiro.ishino@gmail.com>
+# Note: All comments except these top lines will be written in Japanese. 
+
+import re
+import wx
+
+import views.ViewCreator
+
+from logging import getLogger
+from views.baseDialog import *
+
+class Dialog(BaseDialog):
+	def __init__(self,title,detail,parent=None,validationPattern=None,defaultValue=""):
+		super().__init__("SimpleInputDialog")
+		self.title=title
+		self.detail=detail
+		self.default=defaultValue
+		if parent!=None:
+			self.parent=parent
+		else:
+			self.parent=self.app.hMainView.hFrame
+		self.validationPattern = validationPattern
+
+	def Initialize(self):
+		super().Initialize(self.parent,self.title)
+		self.InstallControls()
+		return True
+
+	def InstallControls(self):
+		"""いろんなwidgetを設置する。"""
+		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.VERTICAL,20,style=wx.ALL|wx.EXPAND,margin=20)
+		self.edit,self.static=self.creator.inputbox(self.detail,defaultValue=self.default,x=-1,style=wx.BORDER_RAISED|wx.TE_DONTWRAP,sizerFlag=wx.EXPAND)
+		self.edit.hideScrollBar(wx.HORIZONTAL)
+
+		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.HORIZONTAL,20,style=wx.ALIGN_RIGHT)
+		self.bOk=self.creator.okbutton(_("ＯＫ"),self.ok)
+		self.bCancel=self.creator.cancelbutton(_("キャンセル"),None)
+
+	def ok(self,event):
+		if self.validationPattern:
+			if not re.fullmatch(self.validationPattern,self.edit.GetLineText(0)):
+				return
+		event.Skip()
+
+	def GetData(self):
+		return self.edit.GetLineText(0)
---
+++ views/main.py
@@ -0,0 +1,134 @@
+﻿# -*- coding: utf-8 -*-
+#main view
+#Copyright (C) 2019 Yukio Nozawa <personal@nyanchangames.com>
+#Copyright (C) 2019-2021 yamahubuki <itiro.ishino@gmail.com>
+
+import wx
+
+import constants
+import globalVars
+import update
+import menuItemsStore
+
+from .base import *
+from simpleDialog import *
+
+from views import globalKeyConfig
+from views import sample
+from views import settingsDialog
+from views import versionDialog
+
+class MainView(BaseView):
+	def __init__(self):
+		super().__init__("mainView")
+		self.log.debug("created")
+		self.events=Events(self,self.identifier)
+		title=constants.APP_NAME
+		super().Initialize(
+			title,
+			self.app.config.getint(self.identifier,"sizeX",800,400),
+			self.app.config.getint(self.identifier,"sizeY",600,300),
+			self.app.config.getint(self.identifier,"positionX",50,0),
+			self.app.config.getint(self.identifier,"positionY",50,0)
+		)
+		self.InstallMenuEvent(Menu(self.identifier),self.events.OnMenuSelect)
+
+
+class Menu(BaseMenu):
+	def Apply(self,target):
+		"""指定されたウィンドウに、メニューを適用する。"""
+
+		#メニュー内容をいったんクリア
+		self.hMenuBar=wx.MenuBar()
+
+		#メニューの大項目を作る
+		self.hFileMenu=wx.Menu()
+		self.hOptionMenu=wx.Menu()
+		self.hHelpMenu=wx.Menu()
+
+		#ファイルメニュー
+		self.RegisterMenuCommand(self.hFileMenu,[
+				"FILE_EXAMPLE",
+		])
+
+		#オプションメニュー
+		self.RegisterMenuCommand(self.hOptionMenu,{
+			"OPTION_OPTION": events.option,
+			"OPTION_KEY_CONFIG":events.keyConfig,
+		})
+
+		#ヘルプメニュー
+		self.RegisterMenuCommand(self.hHelpMenu,{
+			"HELP_UPDATE": events.checkUpdate,
+			"HELP_VERSIONINFO": events.version,
+		})
+
+		#メニューバーの生成
+		self.hMenuBar.Append(self.hFileMenu,_("ファイル(&F))"))
+		self.hMenuBar.Append(self.hOptionMenu,_("オプション(&O)"))
+		self.hMenuBar.Append(self.hHelpMenu,_("ヘルプ(&H)"))
+		target.SetMenuBar(self.hMenuBar)
+
+class Events(BaseEvents):
+	def example(self,event):
+		d = sample.Dialog()
+		d.Initialize()
+		r = d.Show()
+
+	def option(self,event):
+		d = settingsDialog.Dialog()
+		d.Initialize()
+		d.Show()
+
+	def keyConfig(self,event):
+		if self.setKeymap(self.parent.identifier,_("ショートカットキーの設定"),filter=keymap.KeyFilter().SetDefault(False,False)):
+			#ショートカットキーの変更適用とメニューバーの再描画
+			self.parent.menu.InitShortcut()
+			self.parent.menu.ApplyShortcut(self.parent.hFrame)
+			self.parent.menu.Apply(self.parent.hFrame)
+
+	def checkUpdate(self,event):
+		update.checkUpdate()
+
+	def version(self,event):
+		d = versionDialog.dialog()
+		d.Initialize()
+		r = d.Show()
+
+	def setKeymap(self, identifier,ttl, keymap=None,filter=None):
+		if keymap:
+			try:
+				keys=keymap.map[identifier.upper()]
+			except KeyError:
+				keys={}
+		else:
+			try:
+				keys=self.parent.menu.keymap.map[identifier.upper()]
+			except KeyError:
+				keys={}
+		keyData={}
+		menuData={}
+		for refName in defaultKeymap.defaultKeymap[identifier].keys():
+			title=menuItemsDic.getValueString(refName)
+			if refName in keys:
+				keyData[title]=keys[refName]
+			else:
+				keyData[title]=_("なし")
+			menuData[title]=refName
+
+		d=globalKeyConfig.Dialog(keyData,menuData,[],filter)
+		d.Initialize(ttl)
+		if d.Show()==wx.ID_CANCEL: return False
+
+		keyData,menuData=d.GetValue()
+
+		#キーマップの既存設定を置き換える
+		newMap=ConfigManager.ConfigManager()
+		newMap.read(constants.KEYMAP_FILE_NAME)
+		for name,key in keyData.items():
+			if key!=_("なし"):
+				newMap[identifier.upper()][menuData[name]]=key
+			else:
+				newMap[identifier.upper()][menuData[name]]=""
+		newMap.write()
+		return True
