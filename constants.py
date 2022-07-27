340b0d6e0500bb5cbb37668d51354c30e13ea46a
Hiroki Fujii <hfujii@hisystron.com>
2022-07-27 13:54:39 +0900

    メインビューレイアウト案を実装

--- constants.py
+++ constants.py
@@ -47,3 +47,5 @@ UPDATER_URL = "https://github.com/actlaboratory/updater/releases/download/1.0.0/
 UPDATE_URL = "https://actlab.org/api/checkUpdate"
 UPDATER_VERSION = "1.0.0"
 UPDATER_WAKE_WORD = "hello"
+
+
--- views/main.py
+++ views/main.py
@@ -20,6 +20,20 @@ from views import versionDialog
 
 class MainView(BaseView):
 	def __init__(self):
+		# support file categories
+		self.INPUT_FILE_CATEGORY_DEFAULT = 0
+		self.INPUT_FILE_CATEGORIES = [_("EPUBファイル")]
+		self.OUTPUT_FILE_CATEGORY_DEFAULT = 0
+		self.OUTPUT_FILE_CATEGORIES = [_("音声DAISY図書")]
+
+		# SAPI settings
+		self.SAPI = [_("Microsoft SAPI5"), _("Voicevox")]
+		self.SAPI_DEFAULT = 0
+		self.VOICE_SPEED = [_("遅い"), _("標準"), _("速い"), _("より速い")]
+		self.VOICE_SPEED_DEFAULT = 1
+		self.BREATHING_TIME = [_("速度に最適化"), _("短い"), _("標準"), _("長い")]
+		self.BREATHING_TIME_DEFAULT = 0
+		
 		super().__init__("mainView")
 		self.log.debug("created")
 		self.events=Events(self,self.identifier)
@@ -32,6 +46,29 @@ class MainView(BaseView):
 			self.app.config.getint(self.identifier,"positionY",50,0)
 		)
 		self.InstallMenuEvent(Menu(self.identifier),self.events.OnMenuSelect)
+		self.InstallControls()
+
+	def InstallControls(self):
+		verticalCreator = views.ViewCreator.ViewCreator(self.viewMode, self.creator.GetPanel(), self.creator.GetSizer(), wx.VERTICAL, style=wx.ALL | wx.EXPAND, space=10)
+		horizontalCreator = views.ViewCreator.ViewCreator(self.viewMode, verticalCreator.GetPanel(), verticalCreator.GetSizer(), wx.HORIZONTAL, style=wx.ALL | wx.EXPAND, space=10)
+		self.inputCategoryCombo, tmp = horizontalCreator.combobox(_("変換元データの種類"), state=self.INPUT_FILE_CATEGORY_DEFAULT, selection=self.INPUT_FILE_CATEGORIES)
+		horizontalCreator = views.ViewCreator.ViewCreator(self.viewMode, verticalCreator.GetPanel(), verticalCreator.GetSizer(), wx.HORIZONTAL, style=wx.ALL | wx.EXPAND, space=10)
+		self.inputPathInput, tmp = horizontalCreator.inputbox(_("変換元"), style=0, proportion=1, sizerFlag=wx.ALIGN_CENTER_VERTICAL)
+		self.inputPathInput.hideScrollBar(wx.HORIZONTAL)
+		self.inputBrowseButton = horizontalCreator.button(_("参照"))
+		horizontalCreator = views.ViewCreator.ViewCreator(self.viewMode, verticalCreator.GetPanel(), verticalCreator.GetSizer(), wx.HORIZONTAL, style=wx.ALL | wx.EXPAND, space=10)
+		self.outputCategoryCombo, tmp = horizontalCreator.combobox(_("出力データの種類"), state=self.OUTPUT_FILE_CATEGORY_DEFAULT, selection=self.OUTPUT_FILE_CATEGORIES)
+		horizontalCreator = views.ViewCreator.ViewCreator(self.viewMode, verticalCreator.GetPanel(), verticalCreator.GetSizer(), wx.HORIZONTAL, style=wx.ALL | wx.EXPAND, space=10)
+		self.outputDirInput, tmp = horizontalCreator.inputbox(_("出力先"), style=0, proportion=1, sizerFlag=wx.ALIGN_CENTER_VERTICAL)
+		self.outputDirInput.hideScrollBar(wx.HORIZONTAL)
+		self.outputBrowseButton = horizontalCreator.button(_("参照"))
+		horizontalCreator = views.ViewCreator.ViewCreator(self.viewMode, verticalCreator.GetPanel(), verticalCreator.GetSizer(), wx.HORIZONTAL, style=wx.ALL | wx.EXPAND, space=10)
+		self.sapiCombo, tmp = horizontalCreator.combobox(_("音声エンジン"), state=self.SAPI_DEFAULT, selection=self.SAPI)
+		self.voiceCombo, tmp = horizontalCreator.combobox(_("声"), state=self.OUTPUT_FILE_CATEGORY_DEFAULT, selection=self.OUTPUT_FILE_CATEGORIES)
+		horizontalCreator = views.ViewCreator.ViewCreator(self.viewMode, verticalCreator.GetPanel(), verticalCreator.GetSizer(), wx.HORIZONTAL, style=wx.ALL | wx.EXPAND, space=10)
+		self.voiceSpeedCombo, tmp = horizontalCreator.combobox(_("速度"), state=self.VOICE_SPEED_DEFAULT, selection=self.VOICE_SPEED)
+		self.breathingTimeCombo, tmp = horizontalCreator.combobox(_("息継ぎ"), state=self.BREATHING_TIME_DEFAULT, selection=self.BREATHING_TIME)
+
 
 
 class Menu(BaseMenu):
@@ -53,14 +90,14 @@ class Menu(BaseMenu):
 
 		#オプションメニュー
 		self.RegisterMenuCommand(self.hOptionMenu,{
-			"OPTION_OPTION": events.option,
-			"OPTION_KEY_CONFIG":events.keyConfig,
+			#"OPTION_OPTION": events.option,
+			#"OPTION_KEY_CONFIG":events.keyConfig,
 		})
 
 		#ヘルプメニュー
 		self.RegisterMenuCommand(self.hHelpMenu,{
-			"HELP_UPDATE": events.checkUpdate,
-			"HELP_VERSIONINFO": events.version,
+			#"HELP_UPDATE": events.checkUpdate,
+			#"HELP_VERSIONINFO": events.version,
 		})
 
 		#メニューバーの生成
