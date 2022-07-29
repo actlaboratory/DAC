b90790826fdfb4a8a08abad3404e9fd817aea099
Hiroki Fujii <hfujii@hisystron.com>
2022-07-30 03:01:14 +0900

    SAPI声一覧取得を実装

--- voiceMaker/sapi.py
+++ voiceMaker/sapi.py
@@ -14,3 +14,14 @@ def outputSapiSpeech(text, fileName):
 	except Exception as e:
 		print("%s => %s" %(str(text), str(e)))
 		return False
+
+def getSapiVoices():
+	engine = CreateObject('SAPI.SpVoice')
+	category = CreateObject("SAPI.SpObjectTokenCategory")
+	category.SetID(r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech_OneCore\Voices", False)
+	voices = [t for t in category.EnumerateTokens() ]
+	return voices
+
+if __name__ == "__main__":
+	for v in getSapiVoices():
+		print(v.GetAttribute("Name"))
