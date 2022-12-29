70e619d4423ff309bef76ee101ee527f2ef48bbc
guredora <contact@guredora.com>
2022-12-29 15:14:12 +0900

    fix typo

--- voiceMaker/voicevox.py
+++ voiceMaker/voicevox.py
@@ -12,71 +12,71 @@ from views import voicevoxSettingsDialog
 from .voiceMakerInterface import *
 
 class voicevox(voiceMakerInterface):
-    def generateWave(text, fileName):
-        # カナ処理
-        #cnv = EnglishToKanaConverter(False)
-        #if kanaConvert: text = cnv.process(text)
+	def generateWave(text, fileName):
+		# カナ処理
+		#cnv = EnglishToKanaConverter(False)
+		#if kanaConvert: text = cnv.process(text)
 
-        # Internal Server Error(500)が出ることがあるのでリトライする
-        # （HTTPAdapterのretryはうまくいかなかったので独自実装）
-        # connect timeoutは10秒、read timeoutは3000秒に設定（長文対応）
-        # audio_query
-        query_payload = {"text": text, "speaker": voicevox.getSpeakerId()}
-        for query_i in range(globalVars.app.config.getint("Voicevox", "max_retry", 10)):
-            r = requests.post("http://localhost:50021/audio_query", 
-                params=query_payload, timeout=(10.0, 3000.0))
-            if r.status_code == 200:
-                query_data = r.json()
-                break
-            time.sleep(1)
-        else:
-            raise connectionError("Make audio query faild.")
+		# Internal Server Error(500)が出ることがあるのでリトライする
+		# （HTTPAdapterのretryはうまくいかなかったので独自実装）
+		# connect timeoutは10秒、read timeoutは3000秒に設定（長文対応）
+		# audio_query
+		query_payload = {"text": text, "speaker": voicevox.getSpeakerId()}
+		for query_i in range(globalVars.app.config.getint("Voicevox", "max_retry", 10)):
+			r = requests.post("http://localhost:50021/audio_query", 
+				params=query_payload, timeout=(10.0, 3000.0))
+			if r.status_code == 200:
+				query_data = r.json()
+				break
+			time.sleep(1)
+		else:
+			raise connectionError("Make audio query faild.")
 
-        # synthesis
-        synth_payload = {"speaker": voicevox.getSpeakerId()}
-        for synth_i in range(globalVars.app.config.getint("Voicevox", "max_retry", 10)):
-            r = requests.post("http://localhost:50021/synthesis", params=synth_payload, 
-                data=json.dumps(query_data), timeout=(1000.0, 30000.0))
-            if r.status_code == 200:
-                with open(filename, "wb") as fp:
-                    fp.write(r.content)
-                print(f"{filename} は query={query_i+1}回, synthesis={synth_i+1}回のリトライで正常に保存されました")
-                return True
-            time.sleep(1)
-        else:
-            raise engineError("voicevox speak failed.")
+		# synthesis
+		synth_payload = {"speaker": voicevox.getSpeakerId()}
+		for synth_i in range(globalVars.app.config.getint("Voicevox", "max_retry", 10)):
+			r = requests.post("http://localhost:50021/synthesis", params=synth_payload, 
+				data=json.dumps(query_data), timeout=(1000.0, 30000.0))
+			if r.status_code == 200:
+				with open(filename, "wb") as fp:
+					fp.write(r.content)
+				print(f"{filename} は query={query_i+1}回, synthesis={synth_i+1}回のリトライで正常に保存されました")
+				return True
+			time.sleep(1)
+		else:
+			raise engineError("voicevox speak failed.")
 
-    def getVoicevoxVoices():
-        try: r = requests.get("http://localhost:50021/speakers", timeout=(10.0, 30.0))
-        except Exception as e: raise connectionError("get voicevox speakers failed. " + str(e))
-        if r.status_code == 200:
-            return r.json()
-        else:
-            raise connectionError("Get voicevox speakers failed.")
+	def getVoicevoxVoices():
+		try: r = requests.get("http://localhost:50021/speakers", timeout=(10.0, 30.0))
+		except Exception as e: raise connectionError("get voicevox speakers failed. " + str(e))
+		if r.status_code == 200:
+			return r.json()
+		else:
+			raise connectionError("Get voicevox speakers failed.")
 
 	def getVoiceSelections():
-	    result = voicevox.getVoicevoxVoices()
-	    voices = []
-    	for v in result:
-        	for s in v["styles"]:
-            	voices.append({"name": "%s(%s)" %(v["name"], s["name"]), "id": s["id"]})
-	    return voices
+		result = voicevox.getVoicevoxVoices()
+		voices = []
+		for v in result:
+			for s in v["styles"]:
+				voices.append({"name": "%s(%s)" %(v["name"], s["name"]), "id": s["id"]})
+		return voices
 
-    def getSpeakerId():
-        globalVars.app.config.getint("Voicevox", "voice", 0)
+	def getSpeakerId():
+		globalVars.app.config.getint("Voicevox", "voice", 0)
 
-    def getName():
-        return _("Voicevox")
+	def getName():
+		return _("Voicevox")
 
-    def getSettingDialog():
-        return voicevoxSettingsDialog.Dialog()
+	def getSettingDialog():
+		return voicevoxSettingsDialog.Dialog()
 
-    def validateSettings():
-        voices = voicevox.getVoiceSelections()
-        for v in voices:
-            if v["id"] == globalVars.app.config["Voicevox"]["voice"]:
-                return True
-        return False
+	def validateSettings():
+		voices = voicevox.getVoiceSelections()
+		for v in voices:
+			if v["id"] == globalVars.app.config["Voicevox"]["voice"]:
+				return True
+		return False
 
 if __name__ == "__main__":
-    print(voicevox.getVoicevoxVoices())
+	print(voicevox.getVoicevoxVoices())
